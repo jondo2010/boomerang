f8c6ef1704feb7672be7b51fe283bdb05baf5bbd
John Hughes <jondo2010@gmail.com>
2023-04-10 14:47:37 +0200

    fix

--- boomerang_util/examples/keyboard.rs
+++ boomerang_util/examples/keyboard.rs
@@ -1,9 +1,9 @@
 //! This example shows how to use the `KeyboardEvents` reactor to read keyboard input.
-#[cfg(not(windows))]
-use boomerang_util::keyboard_events::{KeyboardEvents, KeyboardEventsBuilder};
 
 #[cfg(not(windows))]
 fn main() {
+    use boomerang_util::keyboard_events::{KeyboardEvents, KeyboardEventsBuilder};
+
     tracing_subscriber::fmt::init();
     let _ = boomerang::run::build_and_run_reactor::<KeyboardEventsBuilder>(
         "keyboard_events",

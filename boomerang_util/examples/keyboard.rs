62b88b73f63e4c0ff025e8d94cabda3bdca1fb91
John Hughes <jondo2010@gmail.com>
2023-04-10 14:23:07 +0200

    fix

--- boomerang_util/examples/keyboard.rs
+++ boomerang_util/examples/keyboard.rs
@@ -1,6 +1,11 @@
+#![cfg(not(windows))]
+
 //! This example shows how to use the `KeyboardEvents` reactor to read keyboard input.
 //!
 use boomerang_util::keyboard_events::{KeyboardEvents, KeyboardEventsBuilder};
+
+#[cfg(windows)]
+compile_error!("This example does not support Windows");
 
 fn main() {
     tracing_subscriber::fmt::init();
